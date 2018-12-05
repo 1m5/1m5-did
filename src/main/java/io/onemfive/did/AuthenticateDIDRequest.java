b69b6b888c664dac778713cdba7ca3612e7649cd
objectorange <objectorange@protonmail.com>
2018-12-05 15:43:52 -0500

    support json serialization

--- src/main/java/io/onemfive/did/AuthenticateDIDRequest.java
+++ src/main/java/io/onemfive/did/AuthenticateDIDRequest.java
@@ -3,6 +3,8 @@ package io.onemfive.did;
 import io.onemfive.core.ServiceRequest;
 import io.onemfive.data.DID;
 
+import java.util.Map;
+
 public class AuthenticateDIDRequest extends ServiceRequest {
     public static final int DID_REQUIRED = 1;
     public static final int DID_ALIAS_REQUIRED = 2;
@@ -14,4 +16,20 @@ public class AuthenticateDIDRequest extends ServiceRequest {
     public static final int DID_TOKEN_FORMAT_MISMATCH = 8;
 
     public DID did;
+
+    @Override
+    public Map<String, Object> toMap() {
+        Map<String, Object> m = super.toMap();
+        if(did!=null) m.put("did",did.toMap());
+        return m;
+    }
+
+    @Override
+    public void fromMap(Map<String, Object> m) {
+        super.fromMap(m);
+        if(m.get("did")!=null) {
+            did = new DID();
+            did.fromMap((Map<String, Object>)m.get("did"));
+        }
+    }
 }
